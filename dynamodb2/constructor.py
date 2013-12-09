80fc5a1fb5c25cfa0839ab28943aaf66277edf4d
Dr. Blez <drblez@gmail.com>
2013-12-09 23:46:10 +0400

    DynamoDB constructor

--- dynamodb2/constructor.py
+++ dynamodb2/constructor.py
@@ -1,5 +1,21 @@
 __author__ = 'drblez'
 
+"""
+
+    Field('f1', 'value1').field('f2', 'value2').field('f3', 42).field('f4', ['a', 'b', 'c']).field('f5', [1, 2, 3]).dict
+
+    {
+        'f1': {'S': 'value1'),
+        'f2': {'S': 'value2'},
+        'f3': {'N': '42'},
+        'f4': {'SS': ['a', 'b', 'c']},
+        'f5': {'NN': [1, 2, 3]}
+    }
+
+    Update('f3').add(1).and(Update('f4').add(['d'])).and(Update('f5').delete([2, 3])).and(Update('f6').put(0))
+
+"""
+
 
 class EmptyList(Exception):
     pass
